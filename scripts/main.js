70a9e5a8779d61a1be21eaf6fad5f8b7b19c3b96
anthony-rk <anthonyrkowalski@gmail.com>
2020-03-15 15:06:14 -0700

    Add function for the Add New Book button on index.html

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,7 @@
 // https://www.valentinog.com/blog/html-table/
 
 let myLibrary = [];
+// let headerArray = ['Title', 'Author'. "Pages", 'Read'];
 
 // Object Constructor
 function Book(title, author, pages, read) {
@@ -10,22 +11,22 @@ function Book(title, author, pages, read) {
     this.read = read
 }
 
-function addBookToLibray(newBook) {
-    myLibrary.push(newBook);
-}
+addBookToLibray = function(newBook, myArray) {
+    myArray.push(newBook);
+};
 
 Book.prototype.toggleRead = function(book) {
     // Toggle "Read" to "Not Read"
     return book.read === true ? book.read = false : book.read = true;
-}
+};
 
 const book1Q84 = new Book('1Q84', 'Haruki Murakami', 1342, true);
 const bookCleanCode = new Book('Clean Code', 'Robert C. Martin', 407, true);
 const bookPridePrejudice = new Book('Pride and Prejudice', 'Jane Austen', 432, false);
 
-addBookToLibray(book1Q84);
-addBookToLibray(bookCleanCode);
-addBookToLibray(bookPridePrejudice);
+addBookToLibray(book1Q84, myLibrary);
+addBookToLibray(bookCleanCode, myLibrary);
+addBookToLibray(bookPridePrejudice, myLibrary);
 
 // To display the table in the console
 console.table(myLibrary);
@@ -69,6 +70,39 @@ let renderBookInfo = function(inputBook) {
     divRead.innerText = inputBook.read;
 
 };
+// Runder renderBookInfor for the headers
+let renderBookHeader = function(inputBook) {
+    let lineBreak = document.createElement('br');
+
+    let baseNode = document.getElementById("books");
+    let parent = document.createElement('div');
+    parent.classList.add('root');
+    baseNode.append(parent);
+    parent.append(lineBreak);
+
+    // Make 4 divs here, one for Title, Author, Pages, and Read. Use CSS Classes to format
+    let divTitle = document.createElement('div')
+    divTitle.classList.add('headerTitle');
+    parent.append(divTitle);
+
+    let divAuthor = document.createElement('div');
+    divAuthor.classList.add('headerAuthor');
+    parent.append(divAuthor);
+
+    let divPages = document.createElement('div');
+    divPages.classList.add('headerPages');
+    parent.append(divPages);
+
+    let divRead = document.createElement('div');
+    divRead.classList.add('headerRead');
+    parent.append(divRead);
+
+    divTitle.innerText = "Title";
+    divAuthor.innerText = 'Author';
+    divPages.innerText = 'Pages';
+    divRead.innerText = 'Read';
+
+}();
 
 // Run renderBooKInfo for the myLibrary array
 let renderOnPage = function(inputArray) {
@@ -78,3 +112,14 @@ let renderOnPage = function(inputArray) {
         renderBookInfo(inputArray[i]);
     }
 }(myLibrary);
+
+let userAddNewBookFunction = function() {
+    let newBookObject = {};
+    // alert("Hello there!");
+    newBookObject.newTitle = prompt("Please enter new book title:", "War & Peace");
+    newBookObject.newAuthor = prompt("Please enter new book author:", "Leo Tolstoy");
+    newBookObject.newPages = prompt("Please enter new book pages:", "1225");
+    newBookObject.newRead = prompt("Please enter if new book has been read:", "No");
+
+    console.log(newBookObject);
+};
