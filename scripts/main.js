a87f0a06da0fd11211a8e7ad719771f2eec8911e
anthony-rk <anthonyrkowalski@gmail.com>
2020-03-15 13:35:48 -0700

    Update main.js render function

--- scripts/main.js
+++ scripts/main.js
@@ -2,33 +2,21 @@
 
 let myLibrary = [];
 
-var render = function (template, node) {
-	node.innerHTML = template;
-};
-
+// Object Constructor
 function Book(title, author, pages, read) {
-    // Constructor
     this.title = title,
     this.author = author,
     this.pages = pages,
     this.read = read
 }
 
-// Book.prototype.info = function() {
-//     if (read === true) {
-//         return title + " by " + author + ", " + pages + " pages, has been read." ;     
-//     } else {
-//         return title + " by " + author + ", " + pages + " pages, not read yet."; 
-//     }
-// }
+function addBookToLibray(newBook) {
+    myLibrary.push(newBook);
+}
 
 Book.prototype.toggleRead = function(book) {
     // Toggle "Read" to "Not Read"
     return book.read === true ? book.read = false : book.read = true;
-}
-
-function addBookToLibray(newBook) {
-    myLibrary.push(newBook);
 }
 
 const book1Q84 = new Book('1Q84', 'Haruki Murakami', 1342, true);
@@ -39,39 +27,37 @@ addBookToLibray(book1Q84);
 addBookToLibray(bookCleanCode);
 addBookToLibray(bookPridePrejudice);
 
+// To display the table in the console
 console.table(myLibrary);
 
-let tbody = document.getElementById("tbody");
-
 // Need to take myLibrary array and stick each data point under the correct field
-myLibrary.forEach(book => {
-    console.log(book.title);
-});
-  
-function generateTable(table, data) {
-    for (let element of data) {
-        let row = table.insertRow();
-        for (key in element) {
-            let cell = row.insertCell();
-            let text = document.createTextNode(element[key]);
-            cell.appendChild(text);
-        }
-    }
-}
-  
-let table = document.querySelector("table");
-let data = Object.keys(myLibrary[0]);
-//   generateTableHead(table, data);
-generateTable(table, myLibrary);
+// myLibrary.forEach(book => {
+//     console.log(book.title);
+// });
 
-// Array-like object
-for (let i = 0; i < myLibrary.length; i++) {
-    // console.log(Object.getOwnPropertyNames(myLibrary[i]));  
-    Object.getOwnPropertyNames(myLibrary[i]).forEach(
-        function (val, index, array) {
-            console.log(val + ' -> ' + myLibrary[i][val]);
-        }
-    )
-}
+// make render work on 1 entry in the array, and then foreach() it to the rest
+let render = function(inputBook) {
+    let parent = document.getElementById("books");
+    // Make 4 divs here, one for Title, Author, Pages, and Read. Use CSS Classes to format
+    let divTitle = document.createElement('div')
+    divTitle.classList.add('divTitle');
+    parent.append(divTitle);
 
-console.log(tbody);
+    let divAuthor = document.createElement('div');
+    divAuthor.classList.add('divAuthor');
+    parent.append(divAuthor);
+
+    let divPages = document.createElement('div');
+    divPages.classList.add('divPages');
+    parent.append(divPages);
+
+    let divRead = document.createElement('div');
+    divRead.classList.add('divRead');
+    parent.append(divRead);
+
+    divTitle.innerText = inputBook.title;
+    divAuthor.innerText = inputBook.author;
+    divPages.innerText = inputBook.pages;
+    divRead.innerText = inputBook.read;
+
+}(myLibrary[0]);
